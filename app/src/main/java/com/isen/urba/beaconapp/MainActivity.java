daeba73cf4c1022c53bdf436d95986e5dc162647
Romain decoster <romain.decoster.rdc@gmail.com>
2016-05-19 15:06:24 +0200

    Initial commit

---
+++ app/src/main/java/com/isen/urba/beaconapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.isen.urba.beaconapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestToTurnOnBluetooth();
+    }
+
+    private void requestToTurnOnBluetooth() {
+        // Request to put on the Bluetooth :
+        int REQUEST_ENABLE_BT=1;
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            return;
+        }
+        if(!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+}
